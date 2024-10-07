aa9af71ec5a5c9676eeccefc84939dd719127f16
guzmancama <juan.guzman.camacho@correounivalle.edu.co>
2024-10-07 11:07:37 -0500

    PRIMERA BASE DEL PROYECTO CON CREACIONES DE CLASES

--- Miniproject_eventos/src/App.java
+++ Miniproject_eventos/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("hi, World!");
+        Soldado soldado1 = new Soldado("Juan", "123", 2);
+        System.out.println(soldado1.nombresoldado);
+        System.out.println(soldado1.ID);
+        System.out.println(soldado1.rango);
+        
     }
 }
