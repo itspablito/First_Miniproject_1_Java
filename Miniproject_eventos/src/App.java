a826fab9879da17cfb2f39d9a196ccf6703442e7
XEBAX10 <sebastian.barco@correounivalle.edu.co>
2024-10-10 21:15:48 -0500

    creacion de metodos

--- Miniproject_eventos/src/App.java
+++ Miniproject_eventos/src/App.java
@@ -1,12 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("hi, World!");
-        Soldado soldado1 = new Soldado("Juan", "123", 1);
-        Rango rango1 = new Rango(4);
-        System.out.println(rango1.rango);
-        System.out.println(soldado1.nombresoldado);
-        System.out.println(soldado1.ID);
 
+        SoldadoRaso soldado1 = new SoldadoRaso("Juan", 1);
+        
         
     }
+
 }
+
--- Miniproject_eventos/src/Soldados/SoldadoRaso.java
+++ Miniproject_eventos/src/Soldados/SoldadoRaso.java
@@ -1,17 +1,34 @@
 package Soldados;
 
-public class SoldadoRaso {  
+public class SoldadoRaso { 
+    String nombre;
+    int ID;
     
+    public SoldadoRaso(String nuevoNombre, int nuevoID) {
+        nombre = nuevoNombre;
+        ID = nuevoID;
+    }
     
-    
-    
+    public SoldadoRaso() {
+        
+    }
     //@Override void RealizarAccion() {
         //System.out.println("Soy un soldado raso y seguire todas las ordenes que me pidan");   
         
         //lo dejaremos esto prodicinomante para cambiarlo despues con el usuario ya que esto son los metodos a usar
     
     
+
+
+
+    public void presentarse() {
+        System.out.println("Soy el soldado:" + nombre + " y mi ID es:" + ID);
+    }
+
+    public void ordenes() {
+        System.out.println("Soy un soldado raso y seguire todas las ordenes que me pidan");
+
+
+
+    }
 }
-
-
-    
